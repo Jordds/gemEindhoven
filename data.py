e6fdaac600c2f26a345a4bbb6ead9df85ded5b92
Jordds <83397869+Jordds@users.noreply.github.com>
2022-12-05 11:29:30 +0100

    tt

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas as pd
 
-sorama = pd.read_csv('sorama.csv', sep = ';', nrows = 5)
+sorama = pd.read_csv('https://raw.githubusercontent.com/Jordds/gemEindhoven/main/csv_dust_sept.csv', sep = ';', nrows = 5)
 vinotion = pd.read_csv('csv_vinotion_sept.csv', nrows = 5)
 tno = pd.read_csv('csv_dust_sept.csv', nrows = 5)
 
